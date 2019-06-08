5ce57a1d3e9b6a7dd5ef37dd379a5203f323e881
Ali Hassani <hasaniali647@gmail.com>
2019-06-08 11:48:19 +0430

    DataBase

---
+++ AdvaceProject/AdvaceProject/BackEnd/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvaceProject.BackEnd
+{
+    public class UnitOfWork:IDisposable
+    {
+        Store_DBEntities1 db = new Store_DBEntities1();
+
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
